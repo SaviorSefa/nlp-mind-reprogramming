4b67ebbf1cd3c332fe5739fef202c25687c20408
SaviorSefa <61169409+SaviorSefa@users.noreply.github.com>
2025-04-06 22:54:07 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-/** @type {import('next') .NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // This helps prevent the window is not defined error
+  compiler: {
+    styledComponents: true,
+  },
 }
 
 module.exports = nextConfig
