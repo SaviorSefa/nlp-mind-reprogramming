5b72c04e8ae5fa6dd0b9a8522496766f105166a9
SaviorSefa <61169409+SaviorSefa@users.noreply.github.com>
2025-04-06 22:24:04 +1000

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,7 @@
-import '../styles/globals.css'
+import '../styles/accessibility.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
 export default MyApp
-import '../styles/accessibility.css'
-
